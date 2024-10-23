75db5a06d660bf0e88e122e726fa0eeaa009decb
Vincent Kruit <vincent.kruit@dtc.ox.ac.uk>
2024-10-23 15:46:31 +0100

    Incorporated ODE in model class and included initial input/output tests

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -3,38 +3,10 @@
 #
 import numpy as np
 import scipy.integrate
+import matplotlib.pyplot as plt
 
 def dose(t, X):
     return X
-
-def rhs(t, y, Q_p1, V_c, V_p1, CL, X):
-    """
-    Creates the Right-Hand-Side function, rhs, of the ODE specifying the quantity of drugs in the central and periferal compartments. 
-
-    Args:
-        param1: t [h], duration for which the drug is administered.
-        param2: y [ng], initial quantity of drugs in central and periferal compartments.
-        param3: Q_p1 [ng], drug quantity in periferal compartment
-        param4: V_c [ml], volume central compartment.
-        param5: V_p1 [ml], volume periferal compartment.
-        param6: CL [ml/h], clearance rate from central compartment.
-        param6: X [ng/h], rate at which the drugs are administered into the central compartment.
-
-    Returns:
-        A list containing the rhs of the change in qc (drug quantity in central channel [ng]) and qp1 over time.
-
-    Raises:
-        KeyError: Raises an exception if input for parameters 2-6 is not an int or float type.
-    """
-    if  y or Q_p1 or V_c or V_p1 or CL or X != isinstance((int, float)):
-        raise Exception(f"Invalid input.")
-    
-    q_c, q_p1 = y
-    transition = Q_p1 * (q_c / V_c - q_p1 / V_p1)
-    dqc_dt = dose(t, X) - q_c / V_c * CL - transition
-    dqp1_dt = transition
-    return [dqc_dt, dqp1_dt]
-
 class Model:
     """A Pharmokinetic (PK) model
 
@@ -45,27 +17,50 @@ class Model:
         an example paramter
 
     """
-    def __init__(self, num_compartments = 1, dosing_type = 'IV', protocol = None):
-        self.num_compartments = num_compartments
-        self.dosing_type = dosing_type
-        self.protocol = protocol
+    def __init__(self, params):
+        self.name = params['name'] # name of the model
+        self.num_comp = params['num_comp'] # number of compartments to model
+        self.dose_type = params['dose_type']
+        #self.protocol = params['protocol']
+        self.y = params['y'] # [ng], initial quantity of drugs in central and periferal compartments.
+        self.Q_p1 = params['Q_p1'] # [ng], drug quantity in periferal compartment
+        self.V_c = params['V_c'] # [ml], volume central compartment.
+        self.V_p1 = params['V_p1'] # [ml], volume periferal compartment.
+        self.CL = params['CL'] # [ml/h], clearance rate from central compartment.
+        self.X = params['X'] # [ng/h], rate at which the drugs are administered into the central compartment.
+        self.step_size = params['step_size']
+        self.endpoint = params['endpoint']
 
-    def ode_system(self, y, t, params):
-        # Example: y[0] is the central compartment, y[1]..y[n] are peripheral
-        # Modify ODEs depending on the number of compartments
-        # unpack params
-        # dydt
-        # central compartment ODEs dydt[0] = #elimination from central compartment
-        # peripheral compartment ODEs
-            # if num_compartments > 1
-                #dydt[1] = exchange with central compartment
+    def ODE_sys(self, t):
+        """
+        Creates the Right-Hand-Side function, rhs, of the ODE specifying the quantity of drugs in the central and periferal compartments. 
 
-        #return dydt
-        pass
-    
-    def zero_comp(self, t, params):
+        Args:
+            param1: t [h], duration for which the drug is administered.
+
+        Returns:
+            A list containing the rhs of the change in qc (drug quantity in central channel [ng]) and qp1 over time.
+
+        Raises:
+            KeyError: Raises an exception if input for parameters 2-6 is not an int or float type.
+        """
+
+        # if  self.y or self.Q_p1 or self.V_c or self.V_p1 or self.CL or self.X != isinstance((int, float)):
+        #    raise Exception(f"Invalid input.")
+        
+        q_c, q_p1 = self.y
+        dqc_dt = dose(t, self.X) - q_c / self.V_c * self.CL
+
+        if self.num_comp < 2:
+                return dqc_dt
+        else:
+                transition = self.Q_p1 * (q_c / self.V_c - q_p1 / self.V_p1)
+                dqp1_dt = transition
+                return dqc_dt - dqp1_dt
+
+    def solve(self, t):
         # initial conditions
-        # y[0] = np.zeros(self.num_compartments + 1)
+        y0 = np.zeros(self.num_comp + 1)
         # A vector of zeros representing the initial concentration of drug in all compartments
         # unpack arguments of params dict and store in args list.
         # Handle dosing type and protocol
@@ -76,21 +71,36 @@ class Model:
         # # Solve the system of ODEs
         # solution = odeint(self.system_of_odes, y0, t, args=(params,))
         # return solution
-
-        args = [
-            params['Q_p1'], params['V_c'], params['V_p1'], params['CL'], params['X']
-            ]
         # unpack timepoints for which to evaluate the function.
-        step_size, end_point = params['step_size'], params['end_point']
-        t_eval = np.linspace(0, step_size, end_point)
+        t_eval = np.linspace(0, self.step_size, self.endpoint)
         # solve the ODE for the given parameters.
         sol = scipy.integrate.solve_ivp(
-        fun=lambda t, y: rhs(t, y, *args),
+        fun=lambda t, y: self.ODE_sys(t),
         t_span=[t_eval[0], t_eval[-1]],
-        y0=params['y0'], t_eval=t_eval
+        y0=y0, t_eval=t_eval
         )
         return sol
-    
-    def one_comp(self, params):
-            return
+
+# Testing
+model_args = {
+    'name': 'Garry',
+    'num_comp': 1,
+    'dose_type': 'IV',
+    'y': [1, 1],
+    'Q_p1': 1.0,
+    'V_c': 1.0,
+    'V_p1': 1.0,
+    'CL': 1.0,
+    'X': 1.0,
+    'step_size': 1,
+    'endpoint': 10
+}
+
+Garry = Model(model_args)
+res = Garry.solve(t=0)
+print(Garry.name)
+print(res.y)
+plt.plot(res.y[0], res.t)
+plt.plot(res.y[1], res.t)
+plt.show()
 
