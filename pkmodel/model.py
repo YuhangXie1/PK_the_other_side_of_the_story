8636a09cd1e695cf4e8abff06bb044fa282f0aea
Vincent Kruit <vincent.kruit@dtc.ox.ac.uk>
2024-10-23 14:28:57 +0100

    Added docstring for rhs function as well as KeyError test

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -8,6 +8,27 @@ def dose(t, X):
     return X
 
 def rhs(t, y, Q_p1, V_c, V_p1, CL, X):
+    """
+    Creates the Right-Hand-Side function, rhs, of the ODE specifying the quantity of drugs in the central and periferal compartments. 
+
+    Args:
+        param1: t [h], duration for which the drug is administered.
+        param2: y [ng], initial quantity of drugs in central and periferal compartments.
+        param3: Q_p1 [ng], drug quantity in periferal compartment
+        param4: V_c [ml], volume central compartment.
+        param5: V_p1 [ml], volume periferal compartment.
+        param6: CL [ml/h], clearance rate from central compartment.
+        param6: X [ng/h], rate at which the drugs are administered into the central compartment.
+
+    Returns:
+        A list containing the rhs of the change in qc (drug quantity in central channel [ng]) and qp1 over time.
+
+    Raises:
+        KeyError: Raises an exception if input for parameters 2-6 is not an int or float type.
+    """
+    if  y or Q_p1 or V_c or V_p1 or CL or X != isinstance((int, float)):
+        raise Exception(f"Invalid input.")
+    
     q_c, q_p1 = y
     transition = Q_p1 * (q_c / V_c - q_p1 / V_p1)
     dqc_dt = dose(t, X) - q_c / V_c * CL - transition
