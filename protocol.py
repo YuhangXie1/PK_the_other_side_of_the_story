7a3cccb2d2228d902bf91f75067f5a15c9fb7bc7
Isabel Lau <isabel.lau@trinity.ox.ac.uk>
2024-10-23 11:57:24 +0100

    Add yaml template v1

---
+++ protocol.py
@@ -0,0 +1,8 @@
+Input:
+.yaml file (contains parameters for input into the model. And which model to use)
+
+Output:
+dictionary (parses the parameters for input into the model)
+which model chosen
+
+Protocols script for converting a file input into instructions for the Model code.
