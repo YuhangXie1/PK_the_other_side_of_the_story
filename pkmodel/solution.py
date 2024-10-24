ecc7fd763b4615b17875223db54d0d7e238a50ac
Claudia Burn-Leefe <claudia.burn-leefe@diamond.ac.uk>
2024-10-24 11:58:12 +0100

    updated function

--- pkmodel/solution.py
+++ pkmodel/solution.py
@@ -6,35 +6,30 @@ import numpy as np
 import pkmodel as pk
 import os
 
+def graph_output(results, t):
+    """Graph the pharmokinetic model
 
-def run_output():
-    """Run the pharmokinetic model and output the result."""
+    Args:
+        pk.Model object
+        Dose time point (float): t
 
-    model_args = pk.load_parameters(file)
-    res = pk.Model(model_args) # Make instance of model
+    """
+    t = results.solve(t)['t'] # Solve for t values
+    y_vals = results.solve(t)['y'] # Solve for y values
 
-    def graph_output(results, t):
-        """Graph the pharmokinetic model
+   if not os.path.exists('../output'):
+        out_path = os.makedirs('../output') # Create directory to store output
+    else:
+        out_path = "../output"
 
-        Args:
-            pk.Model object
-            Dose time point (float): t
+    for y in y_vals:
+        fig = plt.figure()
+        plt.plot(t, y, label=f"Dose type:{results.dose_type} with {results.num_comp} compartments")
+        plt.ylabel('Drug mass [ng]')
+        plt.xlabel('Time [h]')
+        fig.show()
+        fig.savefig(f"{out_path}\\Plot_{results.name}_{results.num_comp}_comp_{results.dose_type}_dose.png")
 
-        """
-        t = results.solve(t)['t'] # Solve for t values
-        y_vals = results.solve(t)['y'] # Solve for y values
 
-        if not os.path.exists('../output'):
-            out_path = os.makedirs('../output') # Create directory to store output
-        else:
-            out_path = "../output"
-
-        for y in y_vals:
-            fig = plt.figure()
-            plt.plot(t, y, label=f"Dose type:{results.dose_type} with {results.num_comp} compartments")
-            plt.ylabel('Drug mass [ng]')
-            plt.xlabel('Time [h]')
-            fig.show()
-            fig.savefig(f"{out_path}\\Plot_{results.name}_{results.num_comp}_comp_{results.dose_type}_dose.png")
-
+if __name__ == "__main__":
     graph_output(res, 0)
