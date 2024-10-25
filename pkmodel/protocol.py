f262adfb5947916869af0146367072d73512c919
Yuhang Xie <yuhangxie1@gmail.com>
2024-10-25 10:28:14 +0100

    made schema reject volumes that are zero

--- pkmodel/protocol.py
+++ pkmodel/protocol.py
@@ -14,8 +14,8 @@ schema = Schema(
             "dose_type": {"value": And(str, lambda s: s in ("IV", "SC"))}, #checks is str and is IV or SC
             "y": {"value": And(list, [Use(float), lambda n: n>=0])}, #checks and turns into float, and is positive
             "Q_p1": {"value": And(Use(float), lambda n: n>=0)},
-            "V_c": {"value": And(Use(float), lambda n: n>=0)},
-            "V_p1": {"value": And(Use(float), lambda n: n>=0)},
+            "V_c": {"value": And(Use(float), lambda n: n>0)},
+            "V_p1": {"value": And(Use(float), lambda n: n>0)},
             "CL": {"value": And(Use(float), lambda n: n>=0)},
             "X": {"value": And(Use(float), lambda n: n>=0)},
             "num_steps": {"value": And(Use(int), lambda n: n>=0)},
